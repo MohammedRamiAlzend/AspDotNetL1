0d8749b7938b7d29e1a1686192e4802a67949504
DESKTOP-F1RTV5Q\Rami <ramialzend@gmail.com>
2024-09-13 20:22:05 +0300

    First commit

--- FirstProject/Controllers/HomeController.cs
+++ FirstProject/Controllers/HomeController.cs
@@ -8,8 +8,8 @@ public class HomeController : Controller
     public ViewResult Index()
     {
         int hour = DateTime.Now.Hour;
-        string viewModle = hour < 12 ? "Good Morning" : "Good Afternoon";
-        return View("MyView", viewModle);
+        string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+        return View("MyView", viewModel);
     }
 
 
